756b6db7dff1c379814776d97399405743d41902
imcouri <coockieche@gmail.com>
2021-07-19 09:43:19 +0300

    added classifier and basic functions

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+
 from flask import Flask
+from flask import request
 from nudenet import NudeClassifier
+from PIL import Image
+import numpy as np
+import urllib.request
+import io
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    URL = request.args.get('url')
     classifier = NudeClassifier()
-    return 'Hello Sammy!'
+    with urllib.request.urlopen(URL) as url:
+        f = io.BytesIO(url.read())
+    image = Image.open(f)
+    result = classifier.classify(np.array(image))
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
